db12ba447785bc03b9e15474e1ff23fdebd07d97
Abdur Rashid Sawom <asawom250@gmail.com>
2023-07-21 04:16:31 +0600

    Update FirebaseSetup.js

--- firebaseNote/FirebaseSetup.js
+++ firebaseNote/FirebaseSetup.js
@@ -2,17 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBK_ix5MUodJ58Ijf1Y3w7gvEqzB2XI2Jo",
-  authDomain: "toursite-44305.firebaseapp.com",
-  projectId: "toursite-44305",
-  storageBucket: "toursite-44305.appspot.com",
-  messagingSenderId: "343110337449",
-  appId: "1:343110337449:web:c436428c0b6856d031bab8",
-  measurementId: "G-TS7X7MM9DV"
+  apiKey: "AIzaSyCfNXAOzOz3f5ThZRLpTRtXwMdb34TLwN8",
+  authDomain: "dentalportal-dfd85.firebaseapp.com",
+  projectId: "dentalportal-dfd85",
+  storageBucket: "dentalportal-dfd85.appspot.com",
+  messagingSenderId: "124045575514",
+  appId: "1:124045575514:web:793eb31059f40ef96b5f63"
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const auth = getAuth(app);
-export default auth;
+export default app;
